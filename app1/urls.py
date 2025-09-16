eb34839fe9013817966ee92f0adbe67d443c0942
Abdullah Hodan <abdullahhodan448@gmail.com>
2025-09-16 10:10:59 +0600

    clean code and model condition

--- app1/urls.py
+++ app1/urls.py
@@ -5,8 +5,5 @@ from app1.views  import signup ,login_view
 urlpatterns=[
     path('reg/',signup,name='singup'), #normal for (FBV)
     path('login/', login_view, name='login'),
-#     path('reg/'singup.as_view(),name='singup'),  # for (CBV)
-#     path('reg/<int:user_id>/'singup, name='singup'), #int parameter diya 
-#     path('reg/<slug:slug>/'singup, name='singup'), #string 
-#     path('reg/<int:user_id>/<slug:slug>/'singup, name='singup') #multiple
+    
  ]
