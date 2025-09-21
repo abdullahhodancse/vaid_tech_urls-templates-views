11ccbda72f6d90f737dfce29d439942696ea1af5
Abdullah Hodan <abdullahhodan448@gmail.com>
2025-09-21 20:40:49 +0600

    admin customization

--- app1/admin/department_admin.py
+++ app1/admin/department_admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from app1.models.departments_model import Department
 
-admin.site.register(Department)
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display=("id","name","created_at")
+    search_fields=('name',)
+    list_filter=("created_at",)
+    ordering=("id",)
+    list_per_page=20
+
--- app1/models/departments_model.py
+++ app1/models/departments_model.py
@@ -1,8 +1,10 @@
 
 from django.db import models
 
+
 class Department(models.Model):
-    name=models.CharField(max_length=100)  
+    name=models.CharField(max_length=100) 
+    created_at = models.DateTimeField(auto_now_add=True) 
 
 
     def __str__(self):
