45ec186d66f856aa485b0f4c4c551546e633dfbd
Abdullah Hodan <abdullahhodan448@gmail.com>
2025-09-22 18:58:51 +0600

    file upload tracker

--- app1/views/file_view.py
+++ app1/views/file_view.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from app1.models.fileUp_model import UploadFfiles
+from django.db.models import Count
 
 def pdf_cart_view(request):
-    # সব ইউজারের PDF ফাইল দেখাবে
-    pdf_files = UploadFfiles.objects.filter(file__iendswith='.pdf')
     
-    return render(request, 'file_load.html', {'pdf_files': pdf_files})
+    pdf_files = UploadFfiles.objects.filter(file__iendswith='.pdf')
+    user_file_count = UploadFfiles.objects.values('user__username') \
+        .annotate(count=Count('id')) \
+        .order_by('-count')  
+    
+    return render(request, 'file_load.html', {'pdf_files': pdf_files,'user_file_count': user_file_count})
